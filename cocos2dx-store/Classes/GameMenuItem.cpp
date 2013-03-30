cdd43856c22f305f32a28a84c7ba34496b9491e3
Alejandro Reyero <axl@todojuegos.com>
2013-03-30 17:41:05 +0100

    Update GameMenuItem.cpp
    Updated property for Cocos2d-X 2.1.2 compatibility.
    
    WARNING: If applied it won't compile with previous versions

--- cocos2dx-store/Classes/GameMenuItem.cpp
+++ cocos2dx-store/Classes/GameMenuItem.cpp
@@ -29,7 +29,7 @@ GameMenuItem* GameMenuItem::itemWithLabel(CCNode*label, CCObject* target, SEL_Me
 void GameMenuItem::selected()
 {
 	// subclass to change the default action
-	if(m_bIsEnabled)
+	if(m_bEnabled)
 	{
 		CCMenuItem::selected();
 
@@ -52,7 +52,7 @@ void GameMenuItem::selected()
 void GameMenuItem::unselected()
 {
 	// subclass to change the default action
-	if (m_bIsEnabled)
+	if (m_bEnabled)
 	{
 		CCMenuItem::unselected();
 		this->stopActionByTag(kZoomActionTag);
